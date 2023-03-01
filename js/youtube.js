f31d287824f6a6852d357997a5a5ce4a7fe2d682
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-01 22:27:38 +0600

    Update youtube.js

--- js/youtube.js
+++ js/youtube.js
@@ -1,1 +1,11 @@
-console.log('first')
+const youtubeData = async() =>{
+ const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCvrh9k-LFww3n5xrbLdiwE09FLDyPRxvU&type=video`;
+
+//  Fetching the Data from this API;
+const res = await fetch(url);
+const data = await res.json();
+ console.log(data) 
+}
+
+// Calling this function;
+youtubeData();
