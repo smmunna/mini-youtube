8ccb7e085c3bf2cb2bfd80c1a4761901bbb72110
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-01 23:39:21 +0600

    update

--- js/youtube.js
+++ js/youtube.js
@@ -1,14 +1,14 @@
 // Showing the all data containing to this API;
-const youtubeData = async () => {
+const newsData = async () => {
     const url = `https://newsapi.org/v2/everything?q=tesla&from=2023-02-01&sortBy=publishedAt&apiKey=8579cef86cab4dc2af1f112f61a614f9`;
     try {
         //  Fetching the Data from this API;
         const res = await fetch(url);
         const data = await res.json();
-        videoItems(data.articles);
+        newsItems(data.articles);
 
         // Get the regionCode from this Data;
-        document.getElementById('regionCode').innerText = data.regionCode;
+        //document.getElementById('regionCode').innerText = data.regionCode;
 
     } catch (error) {
         console.log(error + 'Find the error from this message.')
@@ -16,15 +16,17 @@ const youtubeData = async () => {
 }
 
 // Get the items array from this API
-const videoItems = (videoItem) =>{
-    console.log(videoItem)
+const newsItems = (news) =>{
+    console.log(news)
 
      // Card Main Div
      const mainDivCard = document.getElementById('mainDivCard');
 
     //  Get all the Items
-    
+    news.forEach(teslaNews=>{
+        console.log(teslaNews)
+    })
 }
 
 // Calling this function;
-youtubeData();
+newsData();
